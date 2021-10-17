8fd5f6975885aa56b41d59f10aed53bfaf5fcfc7
claudia <racp86@gmail.com>
2021-10-17 17:50:20 -0400

    segundo commit correction

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,5 +1,6 @@
 """mysite URL Configuration
 
+
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
